aebfae10909f668750f16fcb1403201b0455a683
Abdulllah-Rizwan <abdullahrizwan354@gmail.com>
2023-11-29 04:57:59 +0500

    Api Errors and Api Response utils

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import { connectDB } from "./db/index.js";
 dotenv.config({ path: './env' })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+app.listen(process.env.port || 3000,()=>{
+        console.log(`Listening on port ${process.env.port}`);
+        app.on("error",(error) => {
+                console.log(`Application unable to talk to DB: ${error}`);
+                throw error;
+        });
+});
+})
+.catch((err) => console.log(`MONGODB connection error ${err}`))
 
 // ;(async () => {
 //     try {
 //         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 //         console.log(`\n DB Connected yay!! DB HOST: ${connectionInstance.connection.host} `);
-//         app.on("error",(error) => {
-//             console.log(`Application unable to talk to DB: ${error}`);
-//             throw error;
-//         })
+        // app.on("error",(error) => {
+        //     console.log(`Application unable to talk to DB: ${error}`);
+        //     throw error;
+        // })
 //     } catch (error) {
 //         console.error("ERROR",error);
 //         process.exit(1);    
---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+export class ApiError extends Error{
+    constructor(statusCode,message="Something went wrong",errors = [],stack=""){
+        super(message);
+        this.message = message;
+        this.data = null;
+        this.success = false;
+        this.statusCode = statusCode;
+        this.errors = errors;
+
+        if(stack){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
