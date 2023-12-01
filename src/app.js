0a6d502b84f0ed50df8f186d8242c241198c4271
Abdulllah-Rizwan <abdullahrizwan354@gmail.com>
2023-12-01 11:22:28 +0500

    Impliminted user register functionality partially

--- src/app.js
+++ src/app.js
@@ -12,3 +12,8 @@ app.use(express.json({limit:"20kb"}));
 app.use(express.urlencoded({extended:true,limit:"20kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Import Routes
+import { userRouter } from './routes/user.routes.js';
+
+app.use("/api/v1/users",userRouter);
